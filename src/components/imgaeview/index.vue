e3df31f81b2f4857669a6e2cb5993cec5bd49401
DaisyGet <1905457445@qq.com>
2026-01-08 20:30:28 +0800

    feat: 商品详情页-蒙层小滑块跟随滑动

--- src/components/imgaeview/index.vue
+++ src/components/imgaeview/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { useMouseInElement } from "@vueuse/core";
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -13,6 +14,40 @@ const avtivelist = ref(0);
 const enterhander = (i) => {
   avtivelist.value = i;
 };
+const left = ref(0);
+const top = ref(0);
+const target = ref(null);
+const { elementX, elementY, isOutside } = useMouseInElement(target);
+watch([elementX, elementY, isOutside], () => {
+  // console.log("xy变化了");
+  // 如果鼠标没有移入到盒子里面 直接不执行后面的逻辑
+  if (isOutside.value) return;
+  // console.log("后续逻辑执行了");
+  // 有效范围内控制滑块距离
+  // 横向
+  if (elementX.value > 100 && elementX.value < 300) {
+    left.value = elementX.value - 100;
+  }
+  // 纵向
+  if (elementY.value > 100 && elementY.value < 300) {
+    top.value = elementY.value - 100;
+  }
+
+  // 处理边界
+  if (elementX.value > 300) {
+    left.value = 200;
+  }
+  if (elementX.value < 100) {
+    left.value = 0;
+  }
+
+  if (elementY.value > 300) {
+    top.value = 200;
+  }
+  if (elementY.value < 100) {
+    top.value = 0;
+  }
+});
 </script>
 
 <template>
@@ -21,7 +56,7 @@ const enterhander = (i) => {
     <div class="middle" ref="target">
       <img :src="imageList[avtivelist]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
